52595b60b837eab517b5548d6c5e67a017aac71c
Sam Kim <samuel.kim.2898@gmail.com>
2025-06-27 11:14:30 -0400

    Use env variable for API base URL

--- frontend/src/lib/helpers.ts
+++ frontend/src/lib/helpers.ts
@@ -1,17 +1,17 @@
 import { Stock } from "@/types/stock";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export function getCompareFunction(stockDifferentiator: keyof Stock) {
   return (stock: Stock, stock2: Stock) => (stock2[stockDifferentiator] as number) - (stock[stockDifferentiator] as number); 
 }
 
 export function getApiEndpoint(symbol: string, currAdapcDays: number, currAdpcDays: number, currAdvDays: number) {
-  //return `http://127.0.0.1:5000/stock/${symbol}?adapcDays=${currAdapcDays}&adpcDays=${currAdpcDays}&advDays=${currAdvDays}`;
-  return `https://stock-tracking-and-analysis.onrender.com/stock/${symbol}?adapcDays=${currAdapcDays}&adpcDays=${currAdpcDays}&advDays=${currAdvDays}`;
+  return `${API_BASE_URL}/stock/${symbol}?adapcDays=${currAdapcDays}&adpcDays=${currAdpcDays}&advDays=${currAdvDays}`;
 }
 
 export function getApiEndpointForMultipleStocks(stocksString: string, currAdapcDays: number, currAdpcDays: number, currAdvDays: number) {
-  //return `http://127.0.0.1:5000/stocks?symbols=${stocksString}&adapcDays=${currAdapcDays}&adpcDays=${currAdpcDays}&advDays=${currAdvDays}`;
-  return `https://stock-tracking-and-analysis.onrender.com/stocks?symbols=${stocksString}&adapcDays=${currAdapcDays}&adpcDays=${currAdpcDays}&advDays=${currAdvDays}`;
+  return `${API_BASE_URL}/stocks?symbols=${stocksString}&adapcDays=${currAdapcDays}&adpcDays=${currAdpcDays}&advDays=${currAdvDays}`;
 }
 
 export function batchStocks(stockList: string[], batchSize = 50) {
